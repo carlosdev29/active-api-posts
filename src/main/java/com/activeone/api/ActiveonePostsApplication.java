469d6de5297e936afd872298c18091c357939e26
carlosdev29 <caluss48@hotmail.com>
2024-02-09 23:27:43 -0500

    Se actualiza repo

---
+++ src/main/java/com/activeone/api/ActiveonePostsApplication.java
@@ -0,0 +1,13 @@
+package com.activeone.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveonePostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveonePostsApplication.class, args);
+	}
+
+}
