991ebaea280b09bbef1f729ef18e8a20163f03fd
carlosdev29 <caluss48@hotmail.com>
2024-02-11 00:22:23 -0500

    Se actualiza repo

--- src/main/java/com/activeone/api/controller/PostController.java
+++ src/main/java/com/activeone/api/controller/PostController.java
@@ -2,8 +2,16 @@ package com.activeone.api.controller;
 
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.activeone.api.service.IPostService;
+import com.activeone.api.service.dto.PostResponseByDTO;
 
 
 
@@ -11,7 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/post")
 public class PostController {
 	
-	//@GetMapping(value = "/getPosts")
+	@Autowired
+	private IPostService service;
+	
+	
+	@GetMapping(value = "/getPost/{idPost}")
+	public PostResponseByDTO getPostById(@PathVariable Integer idPost) {
+		PostResponseByDTO postResponseByDTO = this.service.getPostByIdServ(idPost);
+		return postResponseByDTO;
+	}
+	
+	
+	@GetMapping(value = "/getPost")
+	public List<PostResponseByDTO> getAllPost() {
+		List<PostResponseByDTO>listPost = this.service.getAllPostServ();
+		return listPost;
+	}
+	
+	
 
 
 }
